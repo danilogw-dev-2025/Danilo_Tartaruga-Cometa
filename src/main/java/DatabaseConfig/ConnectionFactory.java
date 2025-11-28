c5e3a31dd8fc6469030dceca5488330480defb86
Danilo Mendes <danilo.araujo@gwsistemas.com.br>
2025-11-28 16:45:38 -0300

    fix: corrigindo problema do banco, trocando de senha

--- src/main/java/DatabaseConfig/ConnectionFactory.java
+++ src/main/java/DatabaseConfig/ConnectionFactory.java
@@ -5,13 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:postgresql://localhost:5432/nomedobanco"; // Alterar o nome do banco de dados
-    private static final String USER = "postgresql";
-    private static final String PASSWORD = "postgresql";
+    private static final String URL = "jdbc:postgresql://localhost:5432/trilhaGW"; // Alterar o nome do banco de dados
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
 
     public static Connection getConnection() {
         try {
+            // FORÇA o carregamento do driver do PostgreSQL
+            Class.forName("org.postgresql.Driver");
+
             return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver do PostgreSQL não encontrado: " + e.getMessage(), e);
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao conectar ao banco de dados", e);
         }
