4a3947406c236e87800a586fa9264ef17bd34313
Danilo Mendes <danilo.araujo@gwsistemas.com.br>
2025-12-01 16:08:54 -0300

    feature: implementando funciobalidades de criar e listar Clientes

--- src/main/java/Model/Cliente.java
+++ src/main/java/Model/Cliente.java
@@ -8,32 +8,32 @@ public class Cliente {
     private String cpf;
     private String dataNascimento;
     private String email;
-    private String senha;
+
 
     public Cliente() {
 
     }
 
-    public Cliente (String codigoCliente, String nome, String cpf, String dataNascimento, String email, String senha) {
+
+    public Cliente (String codigoCliente, String nome, String cpf, String dataNascimento, String email) {
         this.codigoCliente = codigoCliente;
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
         this.email = email;
-        this.senha = senha;
 
     }
 
-    public Cliente (Long idCliente, String codigoCliente, String nome, String cpf, String dataNascimento, String email, String senha) {
+    public Cliente(Long idCliente, String codigoCliente, String nome, String cpf, String dataNascimento, String email) {
         this.idCliente = idCliente;
         this.codigoCliente = codigoCliente;
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
         this.email = email;
-        this.senha = senha;
-        this.idCliente = idCliente;
     }
+
+
 
     public Long getIdCliente() {
         return idCliente;
@@ -83,14 +83,6 @@ public class Cliente {
         this.email = email;
     }
 
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-
-    }
 
     public String toString() {
         return "ID_CLIENTE: " + this.idCliente + ", Codigo do Cliente: " + this.codigoCliente + ", Nome: " + this.nome + ", CPF: " + this.cpf + "," +
--- src/main/java/Servlet/ClienteServlet.java
+++ src/main/java/Servlet/ClienteServlet.java
@@ -1,14 +1,25 @@
 package Servlet;
 
+import DAO.ClienteDAO;
+import Model.Cliente;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/cliente")
+
+@WebServlet("/cliente-servlet")
 public class ClienteServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init(){
+        message = "endpoint-cliente";
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -16,5 +27,38 @@ public class ClienteServlet extends HttpServlet {
 
         resp.setContentType("text/html;charset=UTF-8");
         resp.getWriter().println("<h1>Olá, Tomcat + Gradle + Java 8!</h1>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter  pw = response.getWriter();
+
+        String codigoCliente = request.getParameter("codigoCliente");
+        String nome = request.getParameter("nome");
+        String cpf = request.getParameter("cpf");
+        String dataNascimento = request.getParameter("dataNascimento");
+        String email = request.getParameter("email");
+
+
+        Cliente cliente = new Cliente(
+
+                codigoCliente,
+                nome,
+                cpf,
+                dataNascimento,
+                email
+        );
+
+        ClienteDAO  clienteDAO = new ClienteDAO();
+        clienteDAO.cadastrarCliente(cliente);
+
+        response.sendRedirect(request.getContextPath() + "/listar-clientes");
+    }
+
+    @Override
+    public void destroy () {
+
     }
 }
